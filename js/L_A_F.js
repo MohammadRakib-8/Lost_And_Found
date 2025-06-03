ae882b26f4e0da0aaebc5ce8e7226a8b393a56c8
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-06-03 21:10:19 +0600

    Add js code for the repot item button in the home page

---
+++ js/L_A_F.js
@@ -0,0 +1,13 @@
+
+
+let loginBTN=document.getElementById("adminPageBTN");
+let searchBTN=document.getElementById("search");
+let report_BTN=document.getElementById("reportBTN");
+let view_All_ItemBTN=document.getElementById("viewAllItemBTN");
+let about_US_BTN=document.getElementById("aboutUSBTN");
+let contact_US_BTN=document.getElementById("contactUSBTN");
+
+report_BTN.addEventListener('click',(event)=>{
+
+    window.open("report_page.html","_self");
+});
