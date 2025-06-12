71d42109c8ba320c4eb88988a04c5ac81c454adb
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-06-12 12:04:12 +0600

    Fix some functionalites

--- js/L_A_F.js
+++ js/L_A_F.js
@@ -7,11 +7,25 @@ let view_All_ItemBTN=document.getElementById("viewAllItemBTN");
 let about_US_BTN=document.getElementById("aboutUSBTN");
 let contact_US_BTN=document.getElementById("contactUSBTN");
 
+//Page Button Working Functionality Implementation :)
+
 report_BTN.addEventListener('click',(event)=>{
 
     window.open("report_page.html","_self");
 });
-
 loginBTN.addEventListener('click',(event)=>{
 window.open("login.html","_self");
 });
+view_All_ItemBTN.addEventListener('click',(event)=>{
+window.open("view_found_item.html","_self");
+});
+about_US_BTN.addEventListener('click',(event)=>{
+window.open("about_us.html","_self");
+});
+contact_US_BTN.addEventListener('click',(event)=>{
+window.open("contact_us.html","_self");
+});
+
+// Search button for home page left to implement ----------------
+// -------------------------------
+
